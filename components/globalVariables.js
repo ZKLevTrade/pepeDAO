03e76e083340b9d20eb173ab07428ff99bc82c61
ZKLevTrade <dadalauw@gmail.com>
2023-05-05 17:05:28 +0200

    1

--- components/globalVariables.js
+++ components/globalVariables.js
@@ -1,10 +1,10 @@
 // >> Variables
 const getVariable = {
 	['texts']: {
-		pageTitle: 'PepeDEX',
+		pageTitle: 'PEPE DAO',
 		pageDesc:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-		pageImage: 'https://i.imgur.com/2P3e7L0.png',
+			"",
+		pageImage: '',
 		pageLink: '',
 	},
 
